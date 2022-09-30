1fdb64f40f82c8e6ad0058b8b9cac9ced8c39218
rrwe23 <jhrrt0626@gmail.com>
2022-09-30 11:06:37 +0900

    1100 joo

--- pair/todos/views.py
+++ pair/todos/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from . models import Review
 
 # Create your views here.
+
+
+
+def index(request):
+
+    reviews = Review.objects.all()
+    context = {
+        'reviews' : reviews,
+       
+    }
+
+
+    return render(request,"todos/index.html",context)
+
+    
+
+def new(request):
+    return render(request,"todos/new.html")
